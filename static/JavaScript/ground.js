2e9b627295ef96682355e7fa5070a77d4fd3322f
SATS\kyle.hutchinson <kyle1235@gmail.com>
2018-09-05 11:48:51 -0600

    Added the ground to the collidable objects

--- static/JavaScript/ground.js
+++ static/JavaScript/ground.js
@@ -1,12 +1,15 @@
 function makeGround() {
-    var geometry = new THREE.PlaneGeometry(1000, 1000);
+    var geometry = new THREE.BoxGeometry(1000, 0.1, 1000);
     var texture = new THREE.TextureLoader().load('static/Textures/grass.jpg');
     texture.wrapS = THREE.RepeatWrapping;
     texture.wrapT = THREE.RepeatWrapping;
     texture.repeat.set(500, 500);
     var material = new THREE.MeshPhongMaterial({ map: texture, shininess: 0 });
     var ground = new THREE.Mesh(geometry, material);
-    ground.rotateX(-Math.PI / 2);
+
+    ground.translateY(-0.05);
+
+    walls.push(ground);
 
     scene.add(ground);
 }
